e1b9a76d4b2776c75a420deab7b94a8a678c1c94
yukims19 <yukims19@gmail.com>
2019-01-08 18:58:04 -0800

    Add dependencies

--- spotify-app/webpack.config.js
+++ spotify-app/webpack.config.js
@@ -34,6 +34,15 @@ module.exports = {
           }
         ]
       }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: "babel",
+        query: { presets: ["react", "es2015", "stage-1"] }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   }
 };
