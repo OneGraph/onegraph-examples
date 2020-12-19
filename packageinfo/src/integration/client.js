24f5e98dd084bdac762db9f4dc5e9038d3017206
Robin Weser <contact@weser.io>
2020-12-19 21:26:49 +0100

    push app_id to integration

--- packageinfo/src/integration/client.js
+++ packageinfo/src/integration/client.js
@@ -1,7 +1,7 @@
 import 'isomorphic-fetch'
 import { createClient } from 'urql'
 
-import { APP_ID } from '../../env'
+const APP_ID = process.env.APP_ID
 
 export default createClient({
   url: 'https://serve.onegraph.com/dynamic?show_metrics=true&app_id=' + APP_ID,
